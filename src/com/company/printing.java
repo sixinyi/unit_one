44ff36250981237861b6f8abdc771918c43ae390
yangwe <yangwe@ssfs.org>
2019-09-10 15:07:04 -0400

    9/10

--- src/com/company/printing.java
+++ src/com/company/printing.java
@@ -2,4 +2,6 @@ package com.company;
 
 public class printing {
     public static void main(String[] args) {
+    }
+
 }
---
+++ src/com/company/sep10.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class sep10 {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("14:44");
+        System.out.println("9/10/2019");
+        System.out.println("It is a great\ntime to be a\ncomputer programmer");
+        System.out.println("It is a great");
+        System.out.println("time to be a");
+        System.out.println("computer programmer!");
+    }
+}
