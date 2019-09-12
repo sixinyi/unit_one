3a1c31154791e99d68c92456de3b777412f36091
yangwe <yangwe@ssfs.org>
2019-09-12 08:45:35 -0400

    escape

--- src/com/company/Escape.java
+++ src/com/company/Escape.java
@@ -6,6 +6,12 @@ public class Escape {
         System.out.println("What is the difference between");
         System.out.println("a ' and a \"?  Or between a \" and a \"?");
         System.out.println(" ");
-        System.out.print("One is what we see when we're typing our program.\n The other is what appears on the \"console.\"");
+        System.out.println("One is what we see when we're typing our program.\n The other is what appears on the \"console.\"");
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\\n");
     }
 }
