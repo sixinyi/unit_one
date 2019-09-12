24e0c443d5cb3df022667414ca18a5ea674fdb44
yangwe <yangwe@ssfs.org>
2019-09-12 08:35:12 -0400

    9/10

---
+++ src/com/company/Escape.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Escape {
+
+    public static void main(String[] args) {
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"?  Or between a \" and a \"?");
+        System.out.println(" ");
+        System.out.print("One is what we see when we're typing our program.\n The other is what appears on the \"console.\"");
+    }
+}
