2f9875235923e5308bf563cbeec183bba0c7af16
yangwe <yangwe@ssfs.org>
2019-09-13 10:07:18 -0400

    9/13

--- src/com/company/Face.java
+++ src/com/company/Face.java
@@ -1,7 +1,6 @@
 package com.company;
-
-public class printing {
+public class Face {
     public static void main(String[] args) {
+        System.out.println(" xxxxx ");
     }
-
 }
---
+++ src/com/company/Painting.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Painting {
+    public static void main(String[] args){
+         final int area_per_gallon=350;
+         int length=32;
+         int width=40;
+         int height=16;
+         int area_of_door=28;
+         int number_of_door=2;
+         int area_of_window=15;
+         int number_of_window=4;
+         int total_area= length*width+2*length*height+2*width*height;
+         int area_need_to_be_paint= total_area-number_of_door*area_of_door-number_of_window*area_of_window;
+         int gallons_needed= area_need_to_be_paint/area_per_gallon;
+         System.out.print("you will need " + gallons_needed + "gallons of paint");
+    }
+}
