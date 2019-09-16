d9b5ab8f232bf6c41b5c819cdf967915784357e6
yangwe <yangwe@ssfs.org>
2019-09-16 11:02:16 -0400

    9/16 TotalWithTax

---
+++ src/com/company/PoundsToKilograms.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class PoundsToKilograms {
+    public static void main(String[] args) {
+        final double KG_TO_LBS=2.205;
+        double lbs=115.0;
+        double kg=lbs/KG_TO_LBS;
+        System.out.print("115.0 pounds = "+kg+" kilograms");
+    }
+}
