fb2085169c09d31f69bbc932a3fa93de2d273bc7
yangwe <yangwe@ssfs.org>
2019-09-17 12:27:28 -0400

    9/16 temp

--- src/com/company/TempConversion.java
+++ src/com/company/TempConversion.java
@@ -4,5 +4,8 @@ public class TempConversion {
     public static void main(String[] args) {
         double Fahrenheit;
         double Celsius;
+        Fahrenheit=79;
+        Celsius=(Fahrenheit-32)/(1.8);
+        System.out.println("Celsius = "+Celsius);
     }
 }
