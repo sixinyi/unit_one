28f163b596851d5c078363c036ae176312f9a571
yangwe <yangwe@ssfs.org>
2019-09-17 12:41:39 -0400

    9/17 tax

--- src/com/company/TotalWithTax.java
+++ src/com/company/TotalWithTax.java
@@ -1,12 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class TotalWithTax {
     public static void main(String[] args) {
+        System.out.println("type in the price of the food");
+        Scanner F = new Scanner(System.in);
         final double TAXRATE= 0.0825;
         double price;
         double tax;
         double total;
-        price=52.75;
+        price=F.nextDouble();
         tax=price*TAXRATE;
         total=price+tax;
         System.out.println("Price: $"+price);
