0bc892520e53b60d2c3dc85e2818cf477f389307
yangwe <yangwe@ssfs.org>
2019-09-17 12:58:06 -0400

    9/17 p to k

--- src/com/company/PoundsToKilograms.java
+++ src/com/company/PoundsToKilograms.java
@@ -1,10 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class PoundsToKilograms {
     public static void main(String[] args) {
         final double KG_TO_LBS=2.205;
-        double lbs=115.0;
+        Scanner k= new Scanner(System.in);
+        System.out.println("type Pounds in number");
+
+        double lbs=k.nextDouble();
         double kg=lbs/KG_TO_LBS;
-        System.out.print("115.0 pounds = "+kg+" kilograms");
+        System.out.print("pounds = "+kg+" kilograms");
     }
 }
