fc12913062c0f27eedf7f1baa5baff328622f283
yangwe <yangwe@ssfs.org>
2019-09-17 12:49:04 -0400

    9/17 xy

--- src/com/company/RectangleArea.java
+++ src/com/company/RectangleArea.java
@@ -1,10 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class RectangleArea {
     public static void main(String[] args) {
-    int LENGTH= 14;
-    int WIDTH= 6;
-    int AREA= LENGTH*WIDTH;
-    System.out.print("Area = "+AREA);
+        Scanner x= new Scanner(System.in);
+        Scanner y= new Scanner(System.in);
+        System.out.println("Type in the length in number: ");
+        Double LENGTH = x.nextDouble();
+        System.out.println("Type in the width in number: ");
+        Double WIDTH = y.nextDouble();
+        Double AREA= LENGTH*WIDTH;
+        System.out.print("Area = "+AREA);
     }
 }
