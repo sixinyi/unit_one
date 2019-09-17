06adc7fe996bdd8c44324216cb6f2765ee90441b
yangwe <yangwe@ssfs.org>
2019-09-17 12:35:48 -0400

    9/17 temp type

--- src/com/company/TempConversion.java
+++ src/com/company/TempConversion.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class TempConversion {
     public static void main(String[] args) {
+        System.out.println("type in the temperature in Fahrenheit");
+        Scanner F = new Scanner(System.in);
         double Fahrenheit;
         double Celsius;
-        Fahrenheit=79;
+        Fahrenheit=F.nextDouble();
         Celsius=(Fahrenheit-32)/(1.8);
         System.out.println("Celsius = "+Celsius);
     }
