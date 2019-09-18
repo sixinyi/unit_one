25c9fc18630b523139d27903da0cf590bbb076d6
yangwe <yangwe@ssfs.org>
2019-09-18 13:41:41 -0400

    9/18 FindTheAverage

---
+++ src/com/company/FindTheAverage.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class FindTheAverage {
+    void main(String[] args){
+        Scanner enterv= new Scanner(System.in);
+        System.out.println("Enter four values.");
+        System.out.println("Value 1");
+        double v1=enterv.nextDouble();
+        System.out.println("Value 2");
+        double v2=enterv.nextDouble();
+        System.out.println("Value 3");
+        double v3=enterv.nextDouble();
+        System.out.println("Value 4");
+        double v4=enterv.nextDouble();
+        double outcome=0;
+        outcome += v1;
+        outcome += v2;
+        outcome += v3;
+        outcome += v4;
+        System.out.println("Average = "+ outcome);
+    }
+}
